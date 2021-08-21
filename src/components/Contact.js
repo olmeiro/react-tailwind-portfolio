c9872792ebb8172370960681f4ab249f6f012ae2
Olmeiro Orozco <olme.orozco@gmail.com>
2021-08-21 10:59:10 -0500

    Base portfolio

---
+++ src/components/Contact.js
@@ -0,0 +1,169 @@
+import React from "react";
+import emailjs from "emailjs-com";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+
+export default function Contact(params) {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
+  const handleInputName = (e) => {
+    setName(e.target.value);
+  };
+  const handleInputEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleInputMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_u2u8so9",
+        "template_7j9gj1p",
+        e.target,
+        "user_l3Zwewj30bTjvhxmPUjol"
+      )
+      .then((res) => {
+        console.log(res);
+        MySwal.fire("Message sent!");
+        setName("");
+        setEmail("");
+        setMessage("");
+
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247.96121199562052!2d-75.63420447650888!3d6.079472214848888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e46868ee889645b%3A0xdc23e1d6e5ff0d04!2sCaldas%2C%20Antioquia%2C%20Colombia!5e0!3m2!1sen!2sus!4v1629330254337!5m2!1sen!2sus"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                Caldas, <br />
+                Antioquia, Medellín - Colombia.
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                olme.orozco@gmail.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">313 5257802</p>
+            </div>
+          </div>
+        </div>
+        <form
+          netlify
+          name="contact"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+          onSubmit={sendEmail}
+        >
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+            I´m looking for an opportunity to apply their knowledge and improve
+            yours code skills.
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+              Name
+            </label>
+            <input
+              type="text"
+              value={name}
+              id="name"
+              name="name"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={handleInputName}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              id="email"
+              name="email"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              onChange={handleInputEmail}
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-400"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              value={message}
+              name="message"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              onChange={handleInputMessage}
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            onChange={handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const projects = [
+  {
+    title: "CRM Vinicol Bombeos",
+    subtitle: "Vinicol CRM",
+    description:
+      "Customer relationship management built for Vinicol Bombeos in 2020. Great experience where I learnt a lot javascript and Laravel framework.",
+    image: "./Vinicol.png",
+  },
+  {
+    title: "GK Report Generator",
+    subtitle: "Gatekeeper helper",
+    description:
+      "GkReport Generator is a tool that help to gatekeepers in their process of hiring, I learned a lot about google Scripts and sheets documents in Drive.",
+    image: "./GKReport.PNG",
+  },
+  {
+    title: "Personal Portfolio",
+    subtitle: "HTML CSS JS Portfolio",
+    description:
+      "Building a personal portfolio always it'll be a challenge, in this case I tried to build my portfolio only with trident: HTML-CSS and Jasvascript.",
+    image: "./personalPortfolio.png",
+  },
+  {
+    title: "GitHub Portfolio",
+    subtitle: "GitHub Profile",
+    description: "A good option to know a litte about me!",
+    image: "./gitHub.png",
+    link: "https://github.com/olmeiro",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Typescript",
+  "Boostrap",
+  "Node.js",
+  "React",
+  "Git - GitHub",
+];
